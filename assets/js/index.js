4993856531f9a83adf9b85ad8d61e0fc17f073e2
codercj <chenhuijia0528@163.com>
2022-06-01 12:46:21 +0800

    完成了主页功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,45 @@
 $(function(){
     getUserInfo();
+    // 点击退出按钮实现退出功能
+    var layer = layui.layer;
+    $('#btnLogout').on('click', function() {
+        layer.confirm('确认退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            // 关闭confirm询问框
+            layer.close(index);
+          });
+    })
 });
 function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem('token') || ''
-        },
-        succsess: function(res) {
-            console.log(res);
+        
+        success: function(res) {
+            if(res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvadar(res.data)
         }
+        // complete: function(res) {
+        //     if(res.responseJSON.status ===1 && res.responseJSON.message === '身份认证失败！') {
+        //         localStorage.removeItem('token');
+        //         location.href = '/login.html';
+        //     }
+        // }
     })
+}
+function renderAvadar(user) {
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    if(user.user_pic !== null) {
+        $('.layui-nav-img').attr('src',user.user_pic).show();
+        $('.text-avatar').hide();
+    }else {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
 }
